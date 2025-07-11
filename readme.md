30316b3bb2879d74f05887281f3023e86f0a5615
Tim Hanewich <tahanewich@live.com>
2025-07-11 12:52:56 -0400

    Added Captured datetime

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 SQL table creation:
 ```
-create table AirQualityReading(ID int IDENTITY(0, 1) PRIMARY KEY, Location tinyint, Temperature tinyint, Humidity tinyint, AQI tinyint, TVOC smallint, ECO2 smallint)
+create table AirQualityReading(ID int IDENTITY(0, 1) PRIMARY KEY, Captured DateTime, Location tinyint, Temperature tinyint, Humidity tinyint, AQI tinyint, TVOC smallint, ECO2 smallint)
 ```
--- src-dotnet/Program.cs
+++ src-dotnet/Program.cs
@@ -95,7 +95,7 @@ namespace TestingSerial
 
 		public static void UploadAirQualityReading(int location, int temperature, int humidity, int aqi, int tvoc, int eco2)
 		{
-			string query = "insert into AirQualityReading (Location, Temperature, Humidity, AQI, TVOC, ECO2) values (" + location.ToString() + ", " + temperature.ToString() + ", " + humidity.ToString() + ", " + aqi.ToString() + ", " + tvoc.ToString() + ", " + eco2.ToString() + ")";
+			string query = "insert into AirQualityReading (Captured, Location, Temperature, Humidity, AQI, TVOC, ECO2) values (" + DateTime.Now.ToString() + ", " + location.ToString() + ", " + temperature.ToString() + ", " + humidity.ToString() + ", " + aqi.ToString() + ", " + tvoc.ToString() + ", " + eco2.ToString() + ")";
 			SqlConnection sqlcon = new SqlConnection(new Settings().SQLConnectionString);
 			SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
 			sqlcon.Open();
