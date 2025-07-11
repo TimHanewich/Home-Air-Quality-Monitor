30cc17416e881dc7121f29094e09c40c9be9c747
Tim Hanewich <tahanewich@live.com>
2025-07-11 11:55:34 -0400

    Added settings

---
+++ src-dotnet/Settings.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace TestingSerial
+{
+    public class Settings
+    {
+        public string SerialID { get; set; } //i.e "/dev/ttyACM0" or "COM6"
+        public string SQLConnectionString { get; set; }
+
+        public Settings()
+        {
+            SerialID = "<SERIAL ID HERE>";
+            SQLConnectionString = "<SQL CONNECTION STRING HERE>";
+        }
+    }
+}
